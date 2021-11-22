4d928f28bc323aadd5849df952d4bf46ea982548
Pischik Sergey <spischik@croc.ru>
2021-11-22 23:02:35 +0300

    Решение задачи 02-components\03-MeetupCover

--- 02-components/03-MeetupCover/MeetupCover.js
+++ 02-components/03-MeetupCover/MeetupCover.js
@@ -2,9 +2,18 @@ import { defineComponent } from './vendor/vue.esm-browser.js';
 
 export default defineComponent({
   name: 'MeetupCover',
-
+  props: {
+    title: {
+      type: String,
+      default: '',
+    },
+    image: {
+      type: String,
+      default: '',
+    },
+  },
   template: `
-    <div class="meetup-cover" style="--bg-url: url('https://course-vue.javascript.ru/api/images/2')">
-        <h1 class="meetup-cover__title">Title</h1>
+    <div class="meetup-cover" :style="image && \`--bg-url: url('\${image}')\`">
+        <h1 class="meetup-cover__title">{{ title }}</h1>
     </div>`,
 });
