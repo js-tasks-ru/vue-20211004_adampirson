7b11d9a41ccc5685cb573dde6645d03496836b80
javascriptru <67475495+jsru-1@users.noreply.github.com>
2021-11-23 12:53:23 +0300

    Added teacher solution for task: MeetupInfo

---
+++ 02-components/04-MeetupInfo/MeetupInfo.js
@@ -0,0 +1,50 @@
+import { defineComponent } from './vendor/vue.esm-browser.js';
+
+export default defineComponent({
+  name: 'MeetupInfo',
+
+  props: {
+    organizer: {
+      type: String,
+      required: true,
+    },
+    place: {
+      type: String,
+      required: true,
+    },
+    date: {
+      type: Number,
+      required: true,
+    },
+  },
+
+  computed: {
+    formattedDate() {
+      return new Date(this.date).toLocaleDateString(navigator.language, {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+      });
+    },
+
+    ISODate() {
+      return new Date(this.date).toISOString().substr(0, 10);
+    },
+  },
+
+  template: `
+    <ul class="meetup-info">
+      <li>
+        <img class="icon meetup-info__icon" alt="icon" src="/assets/icons/icon-user.svg" />
+        {{ organizer }}
+      </li>
+      <li>
+        <img class="icon meetup-info__icon" alt="icon" src="/assets/icons/icon-map.svg" />
+        {{ place }}
+      </li>
+      <li>
+        <img class="icon meetup-info__icon" alt="icon" src="/assets/icons/icon-cal-lg.svg" />
+        <time :datetime="ISODate">{{ formattedDate }}</time>
+      </li>
+    </ul>`,
+});
